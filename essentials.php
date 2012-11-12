e338422fd7657929522e76c5e3b910f6999ed455
Jinu Joseph Daniel <jinujosephdaniel@gmail.com>
2012-11-13 02:05:47 +0530

    added html related functions

--- essentials.php
+++ essentials.php
@@ -56,6 +56,11 @@
 			$this->db($dbName);
 			return $this;
 		 }
+		 public function drop_db($dbName) {
+		    $this->ensureConnection(); 
+			$this->doQuery("DROP DATABASE ".$dbName,'Could not drop database: ' );
+			return true;
+		 }
 		public function db($dbName) {//selects database,
 		    $this->ensureConnection(); 
 		    if(mysql_select_db($dbName)) {
@@ -65,6 +70,15 @@
 			    $ret =  false;
 			}
 			return $ret;
+		}
+		public function get_selected_table() {
+		    return $this->table;
+		}
+		public function get_selected_db() {
+		    return $this->db;
+		}
+		public function get_db_handle() {
+		    return $this->handle;
 		}
 		public function table($tableName) {//selects a table
 		    $this -> table = $tableName;
@@ -138,6 +152,9 @@
 			}
 			return $this;
 		}
+		public function no_of_records() {//returns  number of records matched
+		    return mysql_num_rows($this->result);
+		}
 		public function drop_record($condition = "0") {
 		/*
 		   deletes a record satisfying the condition $condition which is specified as a string
@@ -179,7 +196,6 @@
 	       if($_COOKIE[$cookie]) {
 		        setcookie($cookie, "", time() - 3600);
 			    $ret = true;
-				echo "ssd";
 		    } else {
 		        $ret = false;
 		    }
@@ -193,17 +209,29 @@
 		 }
 	    return $ret;
 	}
-	function isset_cookie($cookie) {
-	   if(isset($_COOKIE[$cookie])) {
-		    $ret = true;
+	function isset_cookie($cookie) {/*
+	      format 1:returns true if the COOKIE variable $cookie is set.Otherwise returns false.
+	      format 2:returns true if all COOKIE variables whose name is stored in the array() $cookie are set , otherwise false 
+	*/
+	    if(!is_array($cookie)) {
+		   $ret = isset($_COOKIE[$cookie]);
 		} else {
-		    $ret = false;
+	       $ret = true;
+	       $i = 0;
+		   $len = sizeof($cookie);
+		   while($i<$len) {
+		       $ky = $cookie[$i++];
+		       $ret = $ret&&isset($_COOKIE[$ky]);
+			   if(!$ret) {
+			       break;
+			   }
+		   }
 		}
-		return $ret;
+	    return  $ret;
 	}
 	function isset_post($var) {/*
-	      format 1:returns true if the POST variable $var is true.Otherwise returns false.
-	      format 2:returns true if all POST variables whose name is stored in the array() $var is true , otherwise false 
+	      format 1:returns true if the POST variable $var is set.Otherwise returns false.
+	      format 2:returns true if all POST variables whose name is stored in the array() $var are set , otherwise false 
 	*/
 	    if(!is_array($var)) {
 		   $ret = isset($_POST[$var]);
@@ -250,5 +278,179 @@
 	function db_str($str){//adds quotes at ends  of a string.
 	    $str = '"'.str_replace('"','\"',$str).'"';
 		return $str;
+	}
+	function line_input($attributes = array()) {
+	 //returns html for text box.attributes is array(attribute1 => value,attribute2 => value....)
+	 $attributes['type'] = 'text';
+	 $ret = html(array('input'=>array('attributes' => $attributes)));
+	 return $ret;
+	}
+	function text_input($attributes = array('value'=>'')) {//returns html for a textarea with attributes array(attr1=>val,...)...attribute named value is innerHTML of textarea
+	    $content = $attributes['value'];
+	    unset($attributes['value']);
+	    return html(array('textarea'=>array('content'=>$content,'attributes' => $attributes)));
+	}
+	function button($attributes = array()) {//returns html of a button with attributes in array(attr1=>val1,...)
+	    $attributes['type'] = 'button';
+        return html(array('input'=>array('attributes'=>$attributes)));	
+	}
+	function submit_button($attributes = array()) {//returns html of a submit  button with attributes in array(attr1=>val1,...)
+	    $attributes['type'] = 'submit';
+        return html(array('input'=>array('attributes'=>$attributes)));	
+	}
+	function reset_button($attributes = array()) {//returns html of a reset button with attributes in array(attr1=>val1,...)
+	    $attributes['type'] = 'reset';
+        return html(array('input'=>array('attributes'=>$attributes)));	
+	}
+	function password($attributes = array()) {//returns html of a password field with attributes in array(attr1=>val1,...)
+	    $attributes['type'] = 'password';
+        return html(array('input'=>array('attributes'=>$attributes)));	
+	}
+	function file_input($attributes = array()) {//returns html of a file upload field with attributes in array(attr1=>val1,...)
+	    $attributes['type'] = 'file';
+        return html(array('input'=>array('attributes'=>$attributes)));	
+	}
+	function checkbox($attributes = array()) {//returns html for checkbox with attributes as array(attr1=>val1,...),checked attribute set to 1 => checkbox is checked
+	    if(isset($attributes['checked'])) {
+		    $checked = $attributes['checked'];
+		    if($checked  == 1) {
+			    $attributes['checked'] = 'checked';
+		    } else {
+			    unset($attributes['checked']);
+			}
+		}
+	    $attributes['type'] = 'checkbox';
+	    return html(array('input'=>array('attributes'=>$attributes)));
+	}
+	function radiobutton($attributes = array()) {//returns html for radio button with attributes as array(attr1=>val1,...),checked attribute set to 1 => radio button is checked
+	    if(isset($attributes['checked'])) {
+		    $checked = $attributes['checked'];
+		    if($checked  == 1) {
+			    $attributes['checked'] = 'checked';
+		    } else {
+			    unset($attributes['checked']);
+			}
+		}
+	    $attributes['type'] = 'radio';
+	    return html(array('input'=>array('attributes'=>$attributes)));
+	}
+	function select($options = array(),$attributes = false) {//returns html for select element with options as array options,attributes as attributes(attr1=>val1,...),checked attribute set to 1 => radio button is checked
+	  //options can be either normal array or associative array.If it is normal array innerHTML and value of option elements will be same,otherwise it will be as array(value1=>innerHTML1,...) 
+	   $optionsList = array();
+		$i = 0;
+		$len = sizeof($options);
+		if(!is_associative($options)) {
+		    while($i < $len) {
+		        $value = $options[$i++];
+			    $optionsList['option_'.$i] = array('content'=>$value,'attributes'=>array('value'=>$value));
+		    }
+		} else {
+		    foreach($options as $actualValue => $displayedValue) {
+			    $optionsList['option_'.$i++] = array('content'=>$displayedValue,'attributes'=>array('value'=>$actualValue));
+			    
+			}
+		}
+		$options = html($optionsList);
+		$select = html(array(
+		              'select' => array(
+					                  'content'=>$options,
+									  'attributes'=>$attributes
+									  )
+						));
+	    return $select;
+	}
+	function form() {
+	/*
+	    returns html for a form,all arguments except last arguments are innerHtml of that inside form(possibly result of any input functions like line_input,text_input etc.)
+		last argument is array(
+		    'url' => the url to which form should be submitted..not necessary,
+			'method' =>GET/POST,default is  GET..not necessary,
+			'attributes' => array(attr1=>val1,attr2=>val2,...)//attributes for the form tag,..not necessary,
+			'submitButton' =>true/false,true=>submit button is shown..,..not necessary.,submit button is included if this key is not given,
+			'resetButton' =>true/false,true=>reset button is shown.,..not necessary,reset button is not  included if this key is not given,
+		)
+	*/
+	    $args = func_get_args();
+		$i = 0;
+		$len = sizeof($args);
+		$inputs ='';
+		while($i < $len-1) {
+		    $inputs = $inputs.$args[$i++];
+			
+		}
+		if(isset($args[$len-1])) {
+		    $attributes = $args[$len-1];
+		    $url = isset($attributes['url'])?$attributes['url']:'';
+			unset($attributes['url']);
+		    $submitEnabled = isset($attributes['submitButton'])&&$attributes['submitButton']==false?false:true;
+			unset($attributes['submitButton']);
+		    $resetEnabled = isset($attributes['resetButton'])&&$attributes['resetButton']==true?true:false;
+			unset($attributes['resetButton']);
+		} else {
+			$url = './';
+			$submitEnabled = true;
+			$resetEnabled = false;
+		}
+		$attributes['action'] = $url;
+		if($submitEnabled) { 
+		    $inputs = $inputs.submit_button(array());
+		}
+		if($resetEnabled) { 
+		    $inputs = $inputs.reset_button(array());
+		}
+		$ret = html(array('form'=>array('content'=>$inputs,'attributes'=>$attributes)));
+		return $ret;
+	}
+	
+    function html($tags) {//generate html tags from array named tags 
+	/*tags[tagName] = array [
+	                content => innerHtml,
+	                attributes => array [
+					    attribute1=>value1,attr2=>value ...
+					]
+				]
+				tags with same name should be indicated as $tagNAme_1,$tagName_2 etc
+	*/
+	    $autoCloseTags = array( 
+		    'area'=>1,
+            'base'=>1,
+            'br'=>1,
+            'col'=>1,
+            'command'=>1,
+            'embed'=>1,
+            'hr'=>1,
+            'img'=>1,
+            'input'=>1,
+            'keygen'=>1,
+            'link'=>1,
+            'meta'=>1,
+            'param'=>1,
+            'source'=>1,
+            'track'=>1,
+            'wbr'=>1
+        );
+		$html = array();
+		$len = sizeof($tags);
+		foreach($tags as $tagName => $details) {
+		    $tagName = strtolower($tagName);
+			$tagName = explode('_',$tagName);
+			$tagName = $tagName[0];
+			$attrList = '';
+			if(isset($details['attributes'])) {
+			    foreach($details['attributes'] as $attr => $value) {
+			        $attrList = $attrList.' '.$attr.'='.db_str($value);
+			    }
+			}
+			$tag = '<'.$tagName.$attrList;
+			if(!isset($autoCloseTags[$tagName])) {
+			    $content = isset($details['content'])?$details['content']:'';
+			    $tag = $tag.'>'.$content.'</'.$tagName.'>';
+			} else {
+			      $tag = $tag.' />';
+			}
+			array_push($html,$tag);
+		}
+		$ret = implode('',$html);
+		return $ret;
 	}
 ?>
